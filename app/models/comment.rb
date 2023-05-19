637de8aaf9f3e742c94acc5a026ccb8cd2a9bd47
Racheal Appiah-kubi <coderacheal@gmail.com>
2023-05-19 16:13:03 +0000

    Fix rubocop offenses

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,8 +1,7 @@
 class Comment < ApplicationRecord
+  # A comment belongs to an author
+  # A comment belongs to a post
 
-    # A comment belongs to an author
-    # A comment belongs to a post
-    
-    belongs_to :author, class_name: 'User', foreign_key: :author_id
-    belongs_to :post
-  end
+  belongs_to :author, class_name: 'User', foreign_key: :author_id
+  belongs_to :post
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,23 +1,21 @@
 class User < ApplicationRecord
+  # A user has may posts,
+  # A user can like many posts
+  # A user can make many comments
 
-    # A user has may posts, 
-    # A user can like many posts 
-    # A user can make many comments 
+  has_many :posts, foreign_key: :author_id
+  has_many :likes, foreign_key: :author_id
+  has_many :comments, foreign_key: :author_id
 
-    has_many :posts, foreign_key: :author_id
-    has_many :likes, foreign_key: :author_id
-    has_many :comments, foreign_key: :author_id
+  after_save :update_post_counter
 
-    after_save :update_post_counter
+  def return_top_three
+    posts.order(created_at: :desc).limit(3)
+  end
 
-    def return_top_3
-        posts.order(created_at: :desc).limit(3)
-    end
+  private
 
-    private
-
-    def update_post_counter
-        update(posts_counter: posts_counter.count)
-    end
-    
+  def update_post_counter
+    update(posts_counter: posts_counter.count)
+  end
 end
