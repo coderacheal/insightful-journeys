c86f1c3ea00965e68b5da5f6da034a7dfdfa39e0
Racheal Appiah-kubi <coderacheal@gmail.com>
2023-05-20 16:17:03 +0000

    Fix rubocop offenses #2

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,20 +1,3 @@
-# class Comment < ApplicationRecord
-#   # A comment belongs to an author
-#   # A comment belongs to a post
-
-#   belongs_to :author, class_name: 'User', foreign_key: :author_id
-#   belongs_to :posts
-
-#   after_save :update_comments_counter
-
-#   private
-
-#   def update_comments_counter
-#     posts.increment!(:comments_counter)
-#   end
-# end
-
-
 class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: :author_id
   belongs_to :post
