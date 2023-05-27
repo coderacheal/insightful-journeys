b2b4dfb6e916435ae2782d54b74cdaea061688fb
Racheal Appiah-kubi <coderacheal@gmail.com>
2023-05-27 19:33:19 +0000

    Complete forms for project

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,4 +9,29 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
     @post_comments = Comment.where(post_id: @post)
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    # create a new post associated to the current_user
+    @post = current_user.posts.create(post_params)
+    @post.author = current_user
+
+    if @post.save
+      # if post saved, redirect to the user's posts and render success message
+      redirect_to user_posts_path(current_user), notice: 'Post created successfully.'
+    else
+      # if post not saved, render new template and render error message
+      render :new, notice: 'Post could not be created.'
+    end
+  end
+
+  private
+
+  # Whitelist and extract the allowed parameters
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
 end
