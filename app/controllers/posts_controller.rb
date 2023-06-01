5186519af866d3d17ac3c09241f2a21da163d8db
Racheal Appiah-kubi <coderacheal@gmail.com>
2023-06-01 21:19:39 +0000

    Resolve n +1 problem

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,8 @@
 class PostsController < ApplicationController
   def index
     @user = User.find(params[:user_id])
-    @user_posts = Post.where(author_id: @user.id)
-    @post_comments = Comment.where(post_id: @user_posts.ids)
+    @user_posts = Post.includes(:comments).where(author_id: @user.id)
+    @post_comments = Comment.includes(:author).where(post_id: @user_posts.ids)
   end
 
   def show
