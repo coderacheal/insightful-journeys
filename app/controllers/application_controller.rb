0940e44237b2aa5711acf1b0f4187f95f6596063
Racheal Appiah-kubi <coderacheal@gmail.com>
2023-06-03 16:24:31 +0000

    Fix migration review requested by reviewer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,18 +1,30 @@
+# class ApplicationController < ActionController::Base
+#     protect_from_forgery with: :exception
+
+#     before_action :configure_permitted_parameters, if: :devise_controller?
+
+#     def sign_out_user
+#       sign_out(current_user)
+#       redirect_to root_path
+#     end
+
+#     protected
+
+#     def configure_permitted_parameters
+#       devise_parameter_sanitizer.permit(:sign_up) { |user| user.permit(:name, :surname, :email, :password)}
+#       devise_parameter_sanitizer.permit(:account_update)
+# { |user| user.permit(:name, :surname, :email, :password, :current_password)}
+#     end
+# end
+
 class ApplicationController < ActionController::Base
-    protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-    before_action :configure_permitted_parameters, if: :devise_controller?
+  protected
 
-    def sign_out_user
-      sign_out(current_user)
-      redirect_to root_path
-    end
-
-    protected
-
-    def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up) { |user| user.permit(:name, :surname, :email, :password)}
-      devise_parameter_sanitizer.permit(:account_update)
-      { |user| user.permit(:name, :surname, :email, :password, :current_password)}
-    end
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name surname email password])
+    devise_parameter_sanitizer.permit(:account_update,
+                                      keys: %i[name surname email password current_password])
+  end
 end
