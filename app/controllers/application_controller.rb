937ac25bd9a52a5e13939eb5937e2f49fdf55132
Racheal Appiah-kubi <coderacheal@gmail.com>
2023-06-03 05:34:09 +0000

    Add devise configuration and able to sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,15 @@
+# class ApplicationController < ActionController::Base
+#   def current_user
+#     User.first!
+#   end
+# end
+
 class ApplicationController < ActionController::Base
-  def current_user
-    User.first!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :photo, :bio])
   end
 end
