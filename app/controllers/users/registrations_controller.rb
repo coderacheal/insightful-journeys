51be154e00c8fa0a44faa633926a3595a3c2526e
Racheal Appiah-kubi <coderacheal@gmail.com>
2023-06-03 09:11:23 +0000

    Fix offenses

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -3,31 +3,43 @@ class Users::RegistrationsController < Devise::RegistrationsController
   before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
-
+  # def new
+  #   super
+  # end
 
   # POST /resource
+  # def create
+  #   super
+  # end
 
-
-  # GET /resource/edit
-
+  # # GET /resource/edit
+  # def edit
+  #   super
+  # end
 
   # PUT /resource
-
+  # def update
+  #   super
+  # end
 
   # DELETE /resource
-
+  # def destroy
+  #   super
+  # end
 
   # GET /resource/cancel
   # Forces the session data which is usually expired after sign
   # in to be expired now. This is useful if the user wants to
   # cancel oauth signing in/up in the middle of the process,
   # removing all OAuth session data.
+  # def cancel
+  #   super
+  # end
 
-
-  def sign_out_user
-    sign_out(current_user)
-    redirect_to 'users/registrations#new'
-  end
+  # def sign_out_user
+  #   sign_out(current_user)
+  #   redirect_to 'users/registrations#new'
+  # end
   # protected
 
   # If you have extra params to permit, append them to the sanitizer.
