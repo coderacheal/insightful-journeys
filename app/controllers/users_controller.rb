78c5abf16ce3e07817598bf1f79d8aeaa0cd72a1
Samuel Amankwaa-Frempong <samankwaa.frempong@gmail.com>
2023-06-03 10:58:58 +0000

    Update users_controller.rb with sign_out condition

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,8 +6,13 @@ class UsersController < ApplicationController
   end
   
   def show
+    if params[:id] == 'sign_out'
+      sign_out(current_user)
+      redirect_to root_path
+    else
       @user = User.find(params[:id])
       @recent_posts = @user.return_top_three
+    end
   end
   
 end
